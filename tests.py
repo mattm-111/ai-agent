93d01ee44777c9df1b2c217ef21be1382263ec37
matt-m-111 <mmarks111@hotmail.com>
2025-08-01 08:46:07 -0400

    added write files

--- tests.py
+++ tests.py
@@ -1,23 +1,23 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
+from functions.write_file import write_file
+
 
 
 
 def test():
 
    
-    result = get_file_content("calculator", "main.py")
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
     print(result)
 
-    result = get_file_content("calculator", "pkg/calculator.py")
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
     print(result)
 
-    result = get_file_content("calculator", "/bin/cat") 
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
     print(result)
 
-    result = get_file_content("calculator", "pkg/does_not_exist.py")
-    print(result)
-
+   
 
 
 #     result = get_files_info("calculator", ".")
